9e41bdb15780b705d43975fceabe8aa13fdb2f24
Kevin <welshk91@gmail.com>
2019-03-08 03:09:47 -0500

    Example of how to call the Application Data Repository. Not a permanent spot for that code...

--- app/src/main/java/com/welshk/pheme/app/MainActivity.kt
+++ app/src/main/java/com/welshk/pheme/app/MainActivity.kt
@@ -2,12 +2,24 @@ package com.welshk.pheme.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.welshk.pheme.R
+import com.welshk.pheme.networking.ApplicationDataRepository
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ApplicationDataRepository.getTopHeadlines(
+            "Apple", "us", null, null,
+            {
+                Toast.makeText(this, "Found " + it.articles.size + " articles", Toast.LENGTH_SHORT).show()
+            },
+            {
+                Toast.makeText(this, "Found " + it.message + " articles", Toast.LENGTH_LONG).show()
+            }
+        )
     }
 }
